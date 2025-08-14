e17da9b1f747d6235fd7e0ccc3f100d6d1355606
mohammed ali almaliky <32299659+mhmdali98@users.noreply.github.com>
2025-08-14 16:16:18 +0300

    Update Sidebar.vue

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -4,7 +4,7 @@
             <div class="h-full bg-white dark:bg-[#0e1726]">
                 <div class="flex items-center justify-between px-4 py-3">
                     <NuxtLink to="/" class="main-logo flex shrink-0 items-center">
-                        <img class="ml-[5px] w-8 flex-none" src="/assets/images/logo.svg" alt="" />
+                        <img class="ml-[5px] w-8 flex-none" src="/assets/images/logo.png" alt="" />
                         <span class="align-middle text-2xl font-semibold ltr:ml-1.5 rtl:mr-1.5 dark:text-white-light lg:inline">Qidah</span>
                     </NuxtLink>
                     <a
